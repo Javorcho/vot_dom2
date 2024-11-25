65a64872c72d483665de5396b599be04a198d7f6
Явор Минковски <yavor.v.minkovski.2021@elsys-bg.org>
2024-11-25 08:39:06 +0200

    дамн

--- setup_questions.sql
+++ setup_questions.sql
@@ -1,19 +1,10 @@
-CREATE TABLE questions(
+CREATE TABLE  questions (
     id SERIAL PRIMARY KEY,
-    question TEXT NOT NULL,
-    option_a TEXT NOT NULL,
-    option_b TEXT NOT NULL,
-    option_c TEXT NOT NULL,
-    option_d TEXT NOT NULL,
-    correct_option CHAR(1) NOT NULL
-)
+    question TEXT NOT NULL
+);
 
-SELECT * FROM questions;
-
-DELETE FROM questions;
-
-INSERT INTO questions (question, option_a, option_b, option_c, option_d, correct_option)
-VALUES 
-    ('What is the capital of France?', 'Paris', 'Berlin', 'Madrid', 'Rome', 'A'),
-    ('What is 2 + 2?', '3', '4', '5', '6', 'B'),
-    ('Which planet is known as the Red Planet?', 'Earth', 'Mars', 'Jupiter', 'Saturn', 'B');
+INSERT INTO questions (question)
+VALUES
+('What is the capital of France?'),
+('What is 2 + 2?'),
+('Which planet is known as the Red Planet?');
